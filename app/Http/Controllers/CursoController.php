f307503237ab746433bdc9acb4b6c73bf2f6c4df
Alef Pasqualotto <alef.131204@bento.local>
2022-07-26 09:43:12 -0300

    cursocontroller

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    function index(){
+        echo '3 palavrinhas só';
+    }
+}
