0c0c32fdcd7370df3244bea182a0c4f3e34dda5e
Cesar Torbes <cesar.110205@bento.local>
2022-07-27 09:22:35 -0300

    teste - turma e periodo controller

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -8,15 +8,64 @@ use Illuminate\Support\Facades\DB;
 class CursoController extends Controller
 {
     function index(){
-        $cursos = DB::table('cursos')->SelectRaw('id, nome, nome_reduzido')->orderBy('nome')->get();
+        $cursos = DB::table('cursos')->SelectRaw('id_cursos, nome_cursos, nome_reduzido')
+        ->orderBy('nome_cursos')
+        ->get();
         return view('cursos.index', ['cursos' => $cursos]);
     }
 
     function create(){
-
+        return view('cursos.create');
     }
 
-    function store(){
+    function store(Request $request){
+        $data = $request->all();
+ 
+        unset($data['_token']);
+ 
+        DB::table('cursos')->insert($data);
+ 
+        return redirect('/cursos');
+    }
+
+    function edit($id){
+
+        $cursos = DB::table('cursos')->find($id);
+ 
+        return view('cursos.edit', ['cursos' => $cursos]);
+ 
+    }
+    function update(Request $request){
+        $data = $request->all();
+        unset($data['_token']);
         
+        $id = array_shift($data);
+
+        DB::table('cursos')
+            ->where('id_cursos',$id)
+            ->update(array_intersect_key($data,['nome_cursos'=>1,'nome_reduzido'=>1]));
+ 
+        return redirect('/cursos');
     }
+ 
+    function show($id){
+        $cursos = DB::table('cursos')
+            ->selectRaw("
+                id_cursos,
+                nome_cursos,
+                curso_id
+                ")
+            ->find($id);
+ 
+        return view('cursos.show', ['cursos' => $cursos]);
+    }
+ 
+    function destroy($id){
+ 
+        DB::table('cursos')
+            ->where('id_cursos', $id)
+            ->delete();
+        return redirect('/cursos');
+    }
+
 }
