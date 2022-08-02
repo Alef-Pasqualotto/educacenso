5fde1b2689e227f26b3b21a03348d331a68fb487
Giovanni Zini <giovanni.050804@bento.local>
2022-08-02 09:16:36 -0300

    erro no home ajuda gustavo

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('base.index')
+
+@section('container')
+<a class="btn btn-warning" href="/cursos/index.blade.php">Cursos</a>
+<a class="btn btn-info" href="/peridos/index.blade.php">Períodos</a>
+<a class="btn btn-danger" href="/turmas/index.blade.php">Turmas</a>
+<a class="btn btn-primary" href="/form/index.blade.php">Form</a>
