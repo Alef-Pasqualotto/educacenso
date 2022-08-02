c7f4e1c2b2f78b32b489b2a36989f7c7cd29ad93
Gustavo Orth <gustavo.241004@bento.local>
2022-08-02 08:57:21 -0300

    arrumei o index blade do cursos

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -13,12 +13,12 @@
     <tbody>
         @foreach($cursos as $curso)
             <tr>
-                <td>{{$curso->curso}}</td>
-                <td>{{$curso->curso}}</td>
+                <td>{{$curso->nome_cursos}}</td>
+                <td>{{$curso->nome_reduzido}}</td>    
                 <td>
-                    <a class="btn btn-warning" href="/cursos/{{$curso->id}}/edit">Editar</a>
-                    <a class="btn btn-info" href="/cursos/{{$curso->id}}/show">Ver</a>
-                    <a class="btn btn-danger" href="/cursos/{{$curso->id}}/destroy">Remover</a>
+                    <a class="btn btn-warning" href="/cursos/{{$curso->id_cursos}}/edit">Editar</a>
+                    <a class="btn btn-info" href="/cursos/{{$curso->id_cursos}}/show">Ver</a>
+                    <a class="btn btn-danger" href="/cursos/{{$curso->id_cursos}}/destroy">Remover</a>
                 </td>
             </tr>
         @endforeach
