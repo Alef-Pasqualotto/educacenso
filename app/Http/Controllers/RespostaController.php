10fb932aae84f610fa974b15bc46258adca4f3e0
Alef Pasqualotto <alef.131204@bento.local>
2022-08-03 09:21:11 -0300

    respostacontroller

--- app/Http/Controllers/RespostaController.php
+++ app/Http/Controllers/RespostaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class PeriodoController extends Controller
+class RespostaController extends Controller
 {
     function index(){
         $respostas = DB::table('respostas')
