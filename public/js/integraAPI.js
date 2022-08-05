92de02a3fce88762df9e57a7069b3f223b4e184c
Alef-Pasqualotto <pasqualotto.alef@gmail.com>
2022-08-04 21:52:39 -0300

    api estados

--- public/js/integraAPI.js
+++ public/js/integraAPI.js
@@ -1,14 +1,29 @@
+let select = document.getElementById("uf");
 
-let select = document.getElementById('uf');
-let query = $('#uf');
+function pegaJSON(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success) success(JSON.parse(xhr.responseText));
+            } else {
+                if (error) error(xhr);
+            }
+        }
+    };
+    xhr.open(
+        "GET",
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+        true
+    );
+    xhr.send();
+}
 
-
-
-//   $.getJSON( "https://servicodados.ibge.gov.br/api/v1/localidades/estados", function( data ) {
-//     var options = [];
-//     $.each( data, function( id, nome ) {
-//       options.push( "<option value='" + id + "'>" + nome + "</option>" );
-//     });
-//     select.append(options);
-    
-//   });
+pegaJSON("estados.json", function (data) {    
+    for (let i = 0; i < data.length; i++) {
+        option = document.createElement('option');        
+        option.setAttribute('value', data[i]['id']);
+        option.innerHTML = data[i]['nome'];
+        select.appendChild(option);
+    }
+});
