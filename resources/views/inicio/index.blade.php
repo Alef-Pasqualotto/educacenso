bad6a375a523ca7911f17b46b9e66829c38cad16
GioZini <104837840+GioZini@users.noreply.github.com>
2022-08-23 17:01:42 -0300

    xd
    xd

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -3,6 +3,6 @@
 @section('container')
 
             <div class="container">
-                <div class="masthead-subheading">Bem vindo ao educacenso!</div>
+                <div class="masthead-subheading"><h1>Bem vindo ao educacenso!</h1></div>
             </div>
 @endsection
